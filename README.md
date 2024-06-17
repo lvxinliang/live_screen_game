9550d6ec5901c4a61952ea4d67fdefeaaab8d60f
lvxinliang <lvxinliang@kuaishou.com>
2024-06-17 16:16:52 +0800

    feat: 添加扎气球逻辑

---
+++ README.md
@@ -0,0 +1,7 @@
+## 操作
+1. 进入dycast开源项目：`cd dycast`
+2. 下载依赖：`npm install`
+3. 运行弹幕转发程序：`npm run dev`
+4. 运行弹幕游戏逻辑代码：`python main.py`
+5. 浏览器打开`http://localhost:5173/`，输入房间号，房间号在web端dy地址栏中可以获取`https://live.douyin.com/49910668388`，其中`49910668388`就是房间号，进行连接
+6. 在打开的`http://localhost:5173/`中输入转发地址，第4步的时候会在终端中显示：`ws://localhost:8765`
--- main.py
+++ main.py
@@ -1,30 +1,54 @@
 import serial
 import time
+import asyncio
+import websockets
+import json
 
+ser = None
 def burst_balloon():
-    ser.write(b'G1 Y0 F6000\n')
-    ser.write(b'G1 Y-10 F6000\n')
-    ser.write(b'G1 Y0 F6000\n')
-
-def main_loop():
-    burst_balloon()
-    time.sleep(1)
-
-    burst_balloon()
-    time.sleep(1)
-
-    burst_balloon()
-    time.sleep(1)
-
-if __name__ == '__main__':
-    # Open the serial port
-    ser = serial.Serial('COM4', baudrate=9600)
-
-    # Check if the port is open
+    print('Bursting balloon')
     if ser.is_open:
-        print('Serial port is open')
-        main_loop()
-
+        ser.write(b'G1 Y0 F6000\n')
+        ser.write(b'G1 Y-10 F6000\n')
+        ser.write(b'G1 Y0 F6000\n')
     else:
         print('Failed to open serial port')
-        # Write the string to the serial port
+
+
+def do_action(nickname, gift):
+    # if (gift['name'] == '点亮粉丝团' and gift['count'] >= 1):
+    if (gift['count'] >= 1):
+        print(f'{nickname}送出了{gift["count"]}个{gift["name"]}')
+        for i in range(gift['count']):
+            burst_balloon()
+            # time.sleep(0.1)
+
+def process_msg(msg):
+    try:
+        json_obj = json.loads(msg)
+        if (json_obj['type'] == 'gift'):
+            gift = json_obj['gift']
+            nickname = json_obj['nickname']
+            do_action(nickname, gift)
+    except json.JSONDecodeError as e:
+        print(f'Error decoding JSON: {e}')
+
+async def msg_callback(websocket, path):
+    async for message in websocket:
+        process_msg(message)
+
+def websocket_server():
+    print('WebSocket服务启动成功，可通过 ws://localhost:8765 进行访问')
+    asyncio.get_event_loop().run_until_complete(websockets.serve(msg_callback, 'localhost', 8765))
+    asyncio.get_event_loop().run_forever()
+
+def serial_init():
+    global ser
+    ser = serial.Serial('COM4', baudrate=9600)
+    if ser.is_open:
+        print('Serial port opened')
+    else:
+        print('Failed to open serial port')
+if __name__ == '__main__':
+    serial_init()
+    websocket_server()
